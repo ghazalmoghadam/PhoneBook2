962aa1a18eff5c0455ef3f1d97509ff9b63bfe51
Ghazal Moghadam <moghadam_ghazale@yahoo.com>
2019-09-22 15:37:07 +0330

    Simple phonebook

---
+++ PhoneBook.Infrastructure.DataAccess.EF/PhoneBookDbContext.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using PhoneBook.DomainModel;
+using System;
+
+namespace PhoneBook.Infrastructure.DataAccess.EF
+{
+    public class PhoneBookDbContext: DbContext
+    {
+        public PhoneBookDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().Property(p=>p.PhoneNumber).HasMaxLength(11).IsFixedLength();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ PhoneBook.Presentation.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Application.ApplicationService;
+using PhoneBook.Domain.DomainService;
+using PhoneBook.DomainModel;
+
+namespace PhoneBook.Presentation.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IAppPhoneBookService _appPhoneBookService;
+        public ValuesController(IAppPhoneBookService appPhoneBookService)
+        {
+            _appPhoneBookService = appPhoneBookService;
+        }
+       
+        
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var contacts = _appPhoneBookService.GetAll();
+            return Ok(contacts);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var contact = _appPhoneBookService.GetBy(id);
+            if (contact == null)
+                return NotFound();
+            return Ok(contact);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Contact contact)
+        {
+            if (!ModelState.IsValid)
+             return BadRequest();
+           var returnContact= _appPhoneBookService.Add(contact);
+            return Ok(returnContact);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _appPhoneBookService.Delete(id);
+            return Ok();
+        }
+        [HttpGet("Search")]
+        public IActionResult Search(string searchString)
+        {
+          var contact= _appPhoneBookService.Search(searchString);
+            if (contact == null)
+                return NotFound();
+            return Ok(contact);
+        }
+    }
+}
